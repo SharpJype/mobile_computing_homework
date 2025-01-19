0c1ea94ed6b0ce540546a664557f9b98e9b83624
SharpJype <jyjool10@gmail.com>
2025-01-19 17:39:59 +0200

    update

--- app/src/main/java/com/example/homework_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/homework_kotlin/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.homework_kotlin
 
 
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
 
 import java.math.BigInteger
 import java.util.Random
+
+val FULL_ALPHA: Int = 255 shl 24
+fun randomFullColor(rng:Random):Int {
+    return BigInteger(24, rng).toInt() or FULL_ALPHA
+}
 
 class MainActivity : ComponentActivity() {
     private val rng = Random()
@@ -24,9 +35,12 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun generateBytes() {
-        findViewById<TextView>(R.id.text_byte).text = BigInteger(8, this.rng).toString()
-        findViewById<TextView>(R.id.text_byte1).text = BigInteger(8, this.rng).toString()
-        findViewById<TextView>(R.id.text_byte2).text = BigInteger(8, this.rng).toString()
-        findViewById<TextView>(R.id.text_byte3).text = BigInteger(8, this.rng).toString()
+        val new_view = TextView(this)
+        new_view.text = BigInteger(8, this.rng).toString()
+        new_view.gravity = Gravity.CENTER_VERTICAL or Gravity.CENTER_HORIZONTAL
+        new_view.setTextSize(30F+BigInteger(4, this.rng).toInt())
+        new_view.setTextColor(randomFullColor(this.rng))
+        findViewById<LinearLayout>(R.id.list).addView(new_view)
     }
 }
+
